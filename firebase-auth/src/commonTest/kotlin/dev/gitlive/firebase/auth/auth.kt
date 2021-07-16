fecb1a83c679227ac50e1c40f8b4e497adbe1401
MHayden <mhayden@vikingelectronics.com>
2021-07-16 12:45:33 -0500

    removed commenting on tests

--- firebase-auth/src/commonTest/kotlin/dev/gitlive/firebase/auth/auth.kt
+++ firebase-auth/src/commonTest/kotlin/dev/gitlive/firebase/auth/auth.kt
@@ -14,104 +14,104 @@ expect fun runTest(test: suspend () -> Unit)
 
 class FirebaseAuthTest {
 
-//    @BeforeTest
-//    fun initializeFirebase() {
-//        Firebase
-//            .takeIf { Firebase.apps(context).isEmpty() }
-//            ?.apply {
-//                initialize(
-//                    context,
-//                    FirebaseOptions(
-//                        applicationId = "1:846484016111:ios:dd1f6688bad7af768c841a",
-//                        apiKey = "AIzaSyCK87dcMFhzCz_kJVs2cT2AVlqOTLuyWV0",
-//                        databaseUrl = "https://fir-kotlin-sdk.firebaseio.com",
-//                        storageBucket = "fir-kotlin-sdk.appspot.com",
-//                        projectId = "fir-kotlin-sdk",
-//                        gcmSenderId = "846484016111"
-//                    )
-//                )
-//                Firebase.auth.useEmulator(emulatorHost, 9099)
-//            }
-//    }
-//
-//    @Test
-//    fun testSignInWithUsernameAndPassword() = runTest {
-//        val uid = getTestUid("test@test.com", "test123")
-//        val result = Firebase.auth.signInWithEmailAndPassword("test@test.com", "test123")
-//        assertEquals(uid, result.user!!.uid)
-//    }
-//
-//    @Test
-//    fun testCreateUserWithEmailAndPassword() = runTest {
-//        val email = "test+${Random.nextInt(100000)}@test.com"
-//        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
-//        assertNotEquals(null, createResult.user?.uid)
-//        assertEquals(null, createResult.user?.displayName)
-//        assertEquals(null, createResult.user?.phoneNumber)
-//        assertEquals(email, createResult.user?.email)
-//
-//        val signInResult = Firebase.auth.signInWithEmailAndPassword(email, "test123")
-//        assertEquals(createResult.user?.uid, signInResult.user?.uid)
-//
-//        signInResult.user!!.delete()
-//    }
-//
-//    @Test
-//    fun testFetchSignInMethods() = runTest {
-//        val email = "test+${Random.nextInt(100000)}@test.com"
-//        var signInMethodResult = Firebase.auth.fetchSignInMethodsForEmail(email)
-//        assertEquals(emptyList(), signInMethodResult)
-//        Firebase.auth.createUserWithEmailAndPassword(email, "test123")
-//        signInMethodResult = Firebase.auth.fetchSignInMethodsForEmail(email)
-//        assertEquals(listOf("password"), signInMethodResult)
-//
-//        Firebase.auth.signInWithEmailAndPassword(email, "test123").user!!.delete()
-//    }
-//
-//    @Test
-//    fun testSendEmailVerification() = runTest {
-//        val email = "test+${Random.nextInt(100000)}@test.com"
-//        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
-//        assertNotEquals(null, createResult.user?.uid)
-//        createResult.user!!.sendEmailVerification()
-//
-//        createResult.user!!.delete()
-//    }
-//
-//    @Test
-//    fun sendPasswordResetEmail() = runTest {
-//        val email = "test+${Random.nextInt(100000)}@test.com"
-//        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
-//        assertNotEquals(null, createResult.user?.uid)
-//
-//        Firebase.auth.sendPasswordResetEmail(email)
-//
-//        createResult.user!!.delete()
-//    }
-//
-//    @Test
-//    fun testSignInWithCredential() = runTest {
-//        val uid = getTestUid("test@test.com", "test123")
-//        val credential = EmailAuthProvider.credential("test@test.com", "test123")
-//        val result = Firebase.auth.signInWithCredential(credential)
-//        assertEquals(uid, result.user!!.uid)
-//    }
-//
-//    private suspend fun getTestUid(email: String, password: String): String {
-//        val uid = Firebase.auth.let {
-//            val user = try {
-//                it.createUserWithEmailAndPassword(email, password).user
-//            } catch (e: FirebaseAuthUserCollisionException) {
-//                // the user already exists, just sign in for getting user's ID
-//                it.signInWithEmailAndPassword(email, password).user
-//            }
-//            user!!.uid
-//        }
-//
-//        check(Firebase.auth.currentUser != null)
-//        Firebase.auth.signOut()
-//        check(Firebase.auth.currentUser == null)
-//
-//        return uid
-//    }
+    @BeforeTest
+    fun initializeFirebase() {
+        Firebase
+            .takeIf { Firebase.apps(context).isEmpty() }
+            ?.apply {
+                initialize(
+                    context,
+                    FirebaseOptions(
+                        applicationId = "1:846484016111:ios:dd1f6688bad7af768c841a",
+                        apiKey = "AIzaSyCK87dcMFhzCz_kJVs2cT2AVlqOTLuyWV0",
+                        databaseUrl = "https://fir-kotlin-sdk.firebaseio.com",
+                        storageBucket = "fir-kotlin-sdk.appspot.com",
+                        projectId = "fir-kotlin-sdk",
+                        gcmSenderId = "846484016111"
+                    )
+                )
+                Firebase.auth.useEmulator(emulatorHost, 9099)
+            }
+    }
+
+    @Test
+    fun testSignInWithUsernameAndPassword() = runTest {
+        val uid = getTestUid("test@test.com", "test123")
+        val result = Firebase.auth.signInWithEmailAndPassword("test@test.com", "test123")
+        assertEquals(uid, result.user!!.uid)
+    }
+
+    @Test
+    fun testCreateUserWithEmailAndPassword() = runTest {
+        val email = "test+${Random.nextInt(100000)}@test.com"
+        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
+        assertNotEquals(null, createResult.user?.uid)
+        assertEquals(null, createResult.user?.displayName)
+        assertEquals(null, createResult.user?.phoneNumber)
+        assertEquals(email, createResult.user?.email)
+
+        val signInResult = Firebase.auth.signInWithEmailAndPassword(email, "test123")
+        assertEquals(createResult.user?.uid, signInResult.user?.uid)
+
+        signInResult.user!!.delete()
+    }
+
+    @Test
+    fun testFetchSignInMethods() = runTest {
+        val email = "test+${Random.nextInt(100000)}@test.com"
+        var signInMethodResult = Firebase.auth.fetchSignInMethodsForEmail(email)
+        assertEquals(emptyList(), signInMethodResult)
+        Firebase.auth.createUserWithEmailAndPassword(email, "test123")
+        signInMethodResult = Firebase.auth.fetchSignInMethodsForEmail(email)
+        assertEquals(listOf("password"), signInMethodResult)
+
+        Firebase.auth.signInWithEmailAndPassword(email, "test123").user!!.delete()
+    }
+
+    @Test
+    fun testSendEmailVerification() = runTest {
+        val email = "test+${Random.nextInt(100000)}@test.com"
+        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
+        assertNotEquals(null, createResult.user?.uid)
+        createResult.user!!.sendEmailVerification()
+
+        createResult.user!!.delete()
+    }
+
+    @Test
+    fun sendPasswordResetEmail() = runTest {
+        val email = "test+${Random.nextInt(100000)}@test.com"
+        val createResult = Firebase.auth.createUserWithEmailAndPassword(email, "test123")
+        assertNotEquals(null, createResult.user?.uid)
+
+        Firebase.auth.sendPasswordResetEmail(email)
+
+        createResult.user!!.delete()
+    }
+
+    @Test
+    fun testSignInWithCredential() = runTest {
+        val uid = getTestUid("test@test.com", "test123")
+        val credential = EmailAuthProvider.credential("test@test.com", "test123")
+        val result = Firebase.auth.signInWithCredential(credential)
+        assertEquals(uid, result.user!!.uid)
+    }
+
+    private suspend fun getTestUid(email: String, password: String): String {
+        val uid = Firebase.auth.let {
+            val user = try {
+                it.createUserWithEmailAndPassword(email, password).user
+            } catch (e: FirebaseAuthUserCollisionException) {
+                // the user already exists, just sign in for getting user's ID
+                it.signInWithEmailAndPassword(email, password).user
+            }
+            user!!.uid
+        }
+
+        check(Firebase.auth.currentUser != null)
+        Firebase.auth.signOut()
+        check(Firebase.auth.currentUser == null)
+
+        return uid
+    }
 }
--- firebase-firestore/src/commonTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
+++ firebase-firestore/src/commonTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
@@ -14,143 +14,143 @@ expect fun runTest(test: suspend () -> Unit)
 
 class FirebaseFirestoreTest {
 
-//    @Serializable
-//    data class FirestoreTest(val prop1: String, val time: Double = 0.0)
-//
-//    @BeforeTest
-//    fun initializeFirebase() {
-//        Firebase
-//            .takeIf { Firebase.apps(context).isEmpty() }
-//            ?.apply {
-//                initialize(
-//                    context,
-//                    FirebaseOptions(
-//                        applicationId = "1:846484016111:ios:dd1f6688bad7af768c841a",
-//                        apiKey = "AIzaSyCK87dcMFhzCz_kJVs2cT2AVlqOTLuyWV0",
-//                        databaseUrl = "https://fir-kotlin-sdk.firebaseio.com",
-//                        storageBucket = "fir-kotlin-sdk.appspot.com",
-//                        projectId = "fir-kotlin-sdk",
-//                        gcmSenderId = "846484016111"
-//                    )
-//                )
-//                Firebase.firestore.useEmulator(emulatorHost, 8080)
-//            }
-//    }
-//
-//    @Test
-//    fun testStringOrderBy() = runTest {
-//        setupFirestoreData()
-//        val resultDocs = Firebase.firestore
-//            .collection("FirebaseFirestoreTest")
-//            .orderBy("prop1")
-//            .get()
-//            .documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("aaa", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("ccc", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testFieldOrderBy() = runTest {
-//        setupFirestoreData()
-//
-//        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .orderBy(FieldPath("prop1")).get().documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("aaa", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("ccc", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testStringOrderByAscending() = runTest {
-//        setupFirestoreData()
-//
-//        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .orderBy("prop1", Direction.ASCENDING).get().documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("aaa", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("ccc", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testFieldOrderByAscending() = runTest {
-//        setupFirestoreData()
-//
-//        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .orderBy(FieldPath("prop1"), Direction.ASCENDING).get().documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("aaa", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("ccc", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testStringOrderByDescending() = runTest {
-//        setupFirestoreData()
-//
-//        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .orderBy("prop1", Direction.DESCENDING).get().documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("ccc", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("aaa", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testFieldOrderByDescending() = runTest {
-//        setupFirestoreData()
-//
-//        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .orderBy(FieldPath("prop1"), Direction.DESCENDING).get().documents
-//        assertEquals(3, resultDocs.size)
-//        assertEquals("ccc", resultDocs[0].get("prop1"))
-//        assertEquals("bbb", resultDocs[1].get("prop1"))
-//        assertEquals("aaa", resultDocs[2].get("prop1"))
-//    }
-//
-//    @Test
-//    fun testServerTimestampFieldValue() = runTest {
-//        val doc = Firebase.firestore
-//            .collection("testServerTimestampFieldValue")
-//            .document("test")
-//
-//        doc.set(FirestoreTest.serializer(), FirestoreTest("ServerTimestamp", FieldValue.serverTimestamp))
-//
-//        assertNotEquals(FieldValue.serverTimestamp, doc.get().get("time"))
-//        assertNotEquals(FieldValue.serverTimestamp, doc.get().data(FirestoreTest.serializer()).time)
-//
-//    }
-//
-//
-//    @Test
-//    fun testDocumentAutoId() = runTest {
-//        val doc = Firebase.firestore
-//            .collection("testDocumentAutoId")
-//            .document
-//
-//        doc.set(FirestoreTest.serializer(), FirestoreTest("AutoId"))
-//
-//        val resultDoc = Firebase.firestore
-//            .collection("testDocumentAutoId")
-//            .document(doc.id)
-//            .get()
-//
-//        assertEquals(true, resultDoc.exists)
-//        assertEquals("AutoId", resultDoc.get("prop1"))
-//    }
-//
-//    private suspend fun setupFirestoreData() {
-//        Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .document("one")
-//            .set(FirestoreTest.serializer(), FirestoreTest("aaa"))
-//        Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .document("two")
-//            .set(FirestoreTest.serializer(), FirestoreTest("bbb"))
-//        Firebase.firestore.collection("FirebaseFirestoreTest")
-//            .document("three")
-//            .set(FirestoreTest.serializer(), FirestoreTest("ccc"))
-//    }
+    @Serializable
+    data class FirestoreTest(val prop1: String, val time: Double = 0.0)
+
+    @BeforeTest
+    fun initializeFirebase() {
+        Firebase
+            .takeIf { Firebase.apps(context).isEmpty() }
+            ?.apply {
+                initialize(
+                    context,
+                    FirebaseOptions(
+                        applicationId = "1:846484016111:ios:dd1f6688bad7af768c841a",
+                        apiKey = "AIzaSyCK87dcMFhzCz_kJVs2cT2AVlqOTLuyWV0",
+                        databaseUrl = "https://fir-kotlin-sdk.firebaseio.com",
+                        storageBucket = "fir-kotlin-sdk.appspot.com",
+                        projectId = "fir-kotlin-sdk",
+                        gcmSenderId = "846484016111"
+                    )
+                )
+                Firebase.firestore.useEmulator(emulatorHost, 8080)
+            }
+    }
+
+    @Test
+    fun testStringOrderBy() = runTest {
+        setupFirestoreData()
+        val resultDocs = Firebase.firestore
+            .collection("FirebaseFirestoreTest")
+            .orderBy("prop1")
+            .get()
+            .documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("aaa", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("ccc", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testFieldOrderBy() = runTest {
+        setupFirestoreData()
+
+        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
+            .orderBy(FieldPath("prop1")).get().documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("aaa", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("ccc", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testStringOrderByAscending() = runTest {
+        setupFirestoreData()
+
+        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
+            .orderBy("prop1", Direction.ASCENDING).get().documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("aaa", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("ccc", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testFieldOrderByAscending() = runTest {
+        setupFirestoreData()
+
+        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
+            .orderBy(FieldPath("prop1"), Direction.ASCENDING).get().documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("aaa", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("ccc", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testStringOrderByDescending() = runTest {
+        setupFirestoreData()
+
+        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
+            .orderBy("prop1", Direction.DESCENDING).get().documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("ccc", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("aaa", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testFieldOrderByDescending() = runTest {
+        setupFirestoreData()
+
+        val resultDocs = Firebase.firestore.collection("FirebaseFirestoreTest")
+            .orderBy(FieldPath("prop1"), Direction.DESCENDING).get().documents
+        assertEquals(3, resultDocs.size)
+        assertEquals("ccc", resultDocs[0].get("prop1"))
+        assertEquals("bbb", resultDocs[1].get("prop1"))
+        assertEquals("aaa", resultDocs[2].get("prop1"))
+    }
+
+    @Test
+    fun testServerTimestampFieldValue() = runTest {
+        val doc = Firebase.firestore
+            .collection("testServerTimestampFieldValue")
+            .document("test")
+
+        doc.set(FirestoreTest.serializer(), FirestoreTest("ServerTimestamp", FieldValue.serverTimestamp))
+
+        assertNotEquals(FieldValue.serverTimestamp, doc.get().get("time"))
+        assertNotEquals(FieldValue.serverTimestamp, doc.get().data(FirestoreTest.serializer()).time)
+
+    }
+
+
+    @Test
+    fun testDocumentAutoId() = runTest {
+        val doc = Firebase.firestore
+            .collection("testDocumentAutoId")
+            .document
+
+        doc.set(FirestoreTest.serializer(), FirestoreTest("AutoId"))
+
+        val resultDoc = Firebase.firestore
+            .collection("testDocumentAutoId")
+            .document(doc.id)
+            .get()
+
+        assertEquals(true, resultDoc.exists)
+        assertEquals("AutoId", resultDoc.get("prop1"))
+    }
+
+    private suspend fun setupFirestoreData() {
+        Firebase.firestore.collection("FirebaseFirestoreTest")
+            .document("one")
+            .set(FirestoreTest.serializer(), FirestoreTest("aaa"))
+        Firebase.firestore.collection("FirebaseFirestoreTest")
+            .document("two")
+            .set(FirestoreTest.serializer(), FirestoreTest("bbb"))
+        Firebase.firestore.collection("FirebaseFirestoreTest")
+            .document("three")
+            .set(FirestoreTest.serializer(), FirestoreTest("ccc"))
+    }
 }
